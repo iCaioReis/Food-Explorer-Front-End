2ed01f60c7d0f83b50da95aabea30328348f48c0
iCaioReis <caioreis3186@gmail.com>
2024-05-04 22:23:26 -0300

    Created some files

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { SignUp } from './pages/SingUp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-
-  </React.StrictMode>,
+    <SignUp/>
+  </React.StrictMode>
 )
