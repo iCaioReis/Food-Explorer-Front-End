2a2f0d30c0cd649f89e201fe0df1c4f63d53620a
iCaioReis <caioreis3186@gmail.com>
2024-05-07 21:14:29 -0300

    Stylized form on Sing Up Page

--- src/pages/SingUp/styles.js
+++ src/pages/SingUp/styles.js
@@ -2,9 +2,13 @@ import styled from "styled-components";
 
 export const Container = styled.div`
     height: 100vh;
+    max-width: 1024px;
+
+    margin: 0 auto;
 
     display: flex;
-    align-items: stretch;
+    justify-content: space-around;
+    align-items: center;
 `;
 
 export const Logo = styled.div`
@@ -22,13 +26,32 @@ export const Logo = styled.div`
 
 export const Form = styled.form`
     padding: 64px;
+
     background-color: ${({theme}) => theme.COLORS.DARK_700};
 
-    >span{
+    border-radius: 16px;
+
+    height: 621px;
+
+    > h1 {
+        font-family: "Roboto", sans-serif;
+        font-weight: 700;
+        font-style: normal;
+        font-size: 42px;
+
+        margin-bottom: 32px;
+    }
+
+    > span{
         font-family: "Roboto", sans-serif;
         font-weight: 400;
         font-style: normal;
         font-size: 16px;
         color: ${({theme}) => theme.COLORS.LIGHT_400};
+    }
+
+    input {
+        margin-top: 8px;
+        margin-bottom: 32px;
     }
 `;
