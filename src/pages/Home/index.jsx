161ecb6b4b55a2e36faaa23772ddf15c0817538a
iCaioReis <caioreis3186@gmail.com>
2024-05-16 21:53:23 -0300

    Created Banner in home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,3 +1,5 @@
+import bannerImg from "../../assets/Banners/banner_img.png"
+
 import { Container, Banner } from "./styles";
 
 import { Header } from "../../components/Header";
@@ -6,7 +8,21 @@ export function Home(){
     return(
         <Container>
             <Header/>
-            <Banner/>
+            <Banner>
+                <img src={bannerImg} alt="" />
+                <h1>Sabores inigualáveis</h1>
+                <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
+            </Banner>
+            <Banner>
+                <img src={bannerImg} alt="" />
+                <h1>Sabores inigualáveis</h1>
+                <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
+            </Banner>
+            <Banner>
+                <img src={bannerImg} alt="" />
+                <h1>Sabores inigualáveis</h1>
+                <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
+            </Banner>
         </Container>
     );
 }
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -8,5 +8,25 @@ export const Banner = styled.div`
     width: 1120px;
     height: 260px;
 
-    background-color: ${({ theme }) => theme.COLORS.};
+    margin: 164px auto 0;
+
+    border-radius: 8px;
+
+    background: rgb(9,30,38);
+    background: linear-gradient(0deg, rgba(9,30,38,1) 0%, rgba(0,19,28,1) 100%, rgba(0,212,255,1) 100%);
+
+    position: relative;
+
+    text-align: right;
+    
+
+    > img {
+        position: absolute;
+        top: -146px;
+        left: -50px;
+    }
+
+    > h1 {
+        
+    }
 `;
