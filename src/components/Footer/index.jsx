3c732065e4e164064d86a3f7f8422a666d194816
iCaioReis <caioreis3186@gmail.com>
2024-05-23 20:42:20 -0300

    Stylized Footer component

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,16 +1,18 @@
-import { Container } from "./styles";
+import { Container, Content } from "./styles";
 
 import logoImg from "../../assets/logo_light.png";
 
 export function Footer(){
     return(
         <Container>
-            <div>
-                <img src={logoImg} alt="Logo food explorer"/>
-                <h1>food explorer</h1>
-            </div>
+            <Content>
+                <div>
+                    <img src={logoImg} alt="Logo food explorer"/>
+                    <h1 className="roboto_bigger_bold">food explorer</h1>
+                </div>
             
-            <p>© 2023 - Todos os direitos reservados.</p>
+                <p className="roboto_smaller_regular">© 2023 - Todos os direitos reservados.</p>
+            </Content>
         </Container>
     )
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,5 +1,33 @@
 import styled from "styled-components";
 
 export const Container = styled.footer`
+    height: 77px;
 
+    display: flex;
+    justify-content: center;
+    
+    background-color: ${({ theme }) => theme.COLORS.DARK_600};
+`;
+
+export const Content = styled.div`
+    width: 100%;
+    max-width: 1122px;
+
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    > div {
+        display: flex;
+        align-items: center;
+        gap: 10px;
+
+        h1 {
+            color: ${({ theme }) => theme.COLORS.LIGHT_700};
+        }
+    }
+
+    > p {
+        color: ${({ theme }) => theme.COLORS.LIGHT_200};
+    }
 `;
