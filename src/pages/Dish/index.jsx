5b615af40c21d8817a339ed6e01801f4b1381fd5
iCaioReis <caioreis3186@gmail.com>
2024-05-28 18:44:40 -0300

    Created Dish page

---
+++ src/pages/Dish/index.jsx
@@ -0,0 +1,27 @@
+import { Header } from "../../components/Header";
+
+import { Container } from "./styles";
+
+export function Dish(){
+    return(
+        <Container>
+            <Header/>
+
+            <a href="">{"< Voltar"}</a>
+
+            <div>
+                <img src="" alt="  " />
+
+                <div>
+                    <h1></h1>
+                    <p></p>
+
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </Container>
+    )
+}
