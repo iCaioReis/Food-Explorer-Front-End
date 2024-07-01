95f6d62a32cc9837506e97df28d95ed5d37c35e9
iCaioReis <caioreis3186@gmail.com>
2024-06-30 21:53:36 -0300

    Transforming px to rem

--- src/components/Amount/styles.js
+++ src/components/Amount/styles.js
@@ -3,16 +3,16 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex; 
     align-items: center;
-    gap: 14px;
+    gap: 0.875rem;
 
 > button {
     background: none;
     border: none;
 
-    width: 24px;
-    height: 24px;
+    width: 1.5rem;
+    height: 1.5rem;
 
-    font-size: 24px;
+    font-size: 1.5rem;
     color: ${({ theme }) => theme.COLORS.LIGHT_100};
     }
 `;
