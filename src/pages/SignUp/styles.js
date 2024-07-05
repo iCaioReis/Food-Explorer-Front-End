64fb1c1afa68e17f18ee320cd46f94d54bcc7bc1
iCaioReis <caioreis3186@gmail.com>
2024-07-05 17:09:59 -0300

    Adding responsiveness on SignUp Page

--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -1,32 +1,50 @@
 import styled from "styled-components";
 
+import { DEVICE_BREACKPOINTS } from "../../styles/deviceBreackPoints";
+
 export const Container = styled.div`
     height: 100vh;
     max-width: 1024px;
 
     margin: 0 auto;
+    padding: 1rem;
 
     display: flex;
     justify-content: space-around;
     align-items: center;
+
+    @media (max-width: ${DEVICE_BREACKPOINTS.SM}){
+        flex-direction: column;
+        justify-content: center;
+    }
 `;
 
 export const Logo = styled.div`
     display: flex;
-    gap: 20px;
+    gap: 1.25rem;
     align-items: center;
+
+    > h1 {
+        color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    }
+
+    @media (max-width: ${DEVICE_BREACKPOINTS.SM}){
+       > h1 {
+            font-size: 3rem;
+       } 
+    }
 `;
 
 export const Form = styled.form`
-    width: 476px;
+    width: 29.75rem;
 
-    padding: 64px;
+    padding: 4rem;
 
     display: flex;
     flex-direction: column;
-    gap: 32px;
+    gap: 2rem;
 
-    border-radius: 16px;
+    border-radius: 1rem;
 
     background-color: ${({theme}) => theme.COLORS.DARK_700};
 
@@ -44,4 +62,11 @@ export const Form = styled.form`
         }
     }
 
+    @media (max-width: ${DEVICE_BREACKPOINTS.SM}){
+        background: none;
+
+        > h1 {
+            display: none;
+        }
+    }
 `;
